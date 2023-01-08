5ee33c88e88d7130384f0167934f2fa4fb7efc4c
Ben Ropa <benropa@outlook.com>
2023-01-08 12:51:55 -0600

    gathering data from Weather API

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Setting up Global Variables
+var apiRootUrl = 'https://api.openweathermap.org';
+
 let weather = {
-    "apiKey": "a90173d6616c0e08afdfa728cac2e88b"
-}
+    apiKey: "ecbb3886f8fe3f8258d1cae670ce6cde",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city
+            + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    }
+
+};
